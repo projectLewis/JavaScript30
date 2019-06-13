0722e82613659ed691a318d98e1699dd3faac124
Seth Lewis <lewiscatenary@gmail.com>
2019-06-13 17:02:45 -0500

    completed 01, started 02, separated css into separate file

--- 01 - JavaScript Drum Kit/day1.js
+++ 01 - JavaScript Drum Kit/day1.js
@@ -1,6 +1,18 @@
-window.addEventListener('keydown', (e) => {
+const playAudio = (e) => {
   const audio = document.querySelector(`audio[data-key="${ e.key }"]`);
+  const key = document.querySelector(`.key[data-key="${ e.key }"]`);
   if (!audio) return;
   audio.currentTime = 0; //allows rapid succession when clicked
   audio.play();
-});
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  console.log(this);
+  this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playAudio);
